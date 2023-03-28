8a0da9c789438da22503b087b477b2db89bbef45
yusufEk1n <ekinn.yusuf56@outlook.com>
2023-03-28 05:40:56 +0300

    Set db env.variable and added db service

--- Microservices/CustomerWebApi/Program.cs
+++ Microservices/CustomerWebApi/Program.cs
@@ -1,8 +1,21 @@
+using CustomerWebApi;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+/* Database context dependency injection */
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var dbUserId = Environment.GetEnvironmentVariable("DB_USERID");
+
+var connectionString = $"Data Source={dbHost}; Initial Catalog={dbName}; User ID={dbUserId}; Password={dbPassword}; TrustServerCertificate=True";
+
+builder.Services.AddDbContext<CustomerDbContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
