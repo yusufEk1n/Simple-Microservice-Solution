0f5bd1d34bc2e1799c0be41305af06946d992677
yusufEk1n <ekinn.yusuf56@outlook.com>
2023-03-28 12:30:10 +0300

    Created Product model

---
+++ Microservices/ProductWebApi/Models/Product.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductWebApi.Models
+{
+    [Table(name: "Product")]
+    public class Product
+    {
+        [Key]
+        [Column("product_id")]
+        public int ProductId { get; set; }
+        [Column("product_name")]
+        public string ProductName { get; set; }
+        [Column("product_code")]
+        public string ProductCode { get; set; }
+        [Column("release_date")]
+        public decimal ProductPrice { get; set; }
+    }
+}
