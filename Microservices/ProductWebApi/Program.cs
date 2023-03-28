be30cd54d2c275840d1abf34c8891a5c3b665bdd
yusufEk1n <ekinn.yusuf56@outlook.com>
2023-03-28 12:33:32 +0300

    set mysql db env.variable and added db service

--- Microservices/ProductWebApi/Program.cs
+++ Microservices/ProductWebApi/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using ProductWebApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+/* Database context dependency injection */
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var dbUserId = Environment.GetEnvironmentVariable("DB_USERID");
+
+var connectionString = $"Data Source={dbHost}; Initial Catalog={dbName}; Uid={dbUserId}; Pwd={dbPassword}";
+
+builder.Services.AddDbContext<ProductDbContext>(options => options.UseMySQL(connectionString));
 
 var app = builder.Build();
 
